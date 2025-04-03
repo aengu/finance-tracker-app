faab238366214ff12eee10ed6b979da1f3c7b903
신혜란 <shr19970923@gmail.com>
2025-04-03 18:48:28 +0900

    fix: settings 설정 수정 	- 언어모드: Ko로 수정 	- timezone: 한국으로 수정
    feat: transaction list
    	- 거래 내역 리스트 페이지 추가
    	- nav바에 경로 추가
    	- 향후 필터 기능 추가 예정

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,4 +4,5 @@ from tracker import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path("transactions_list", views.transaction_list, name='transaction_list'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Transaction
 
 # Create your views here.
 def index(request):
     return render(request, 'tracker/index.html')
+
+@login_required
+def transaction_list(request):
+    transactions = Transaction.objects.filter(user = request.user)
+    context = {'transactions' : transactions}
+    return render(request, 'tracker/transaction-list.html', context)
